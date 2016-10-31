00f9c171b568a6b6d65d9531609ce1ffa29728d2
Yamakaky <yamakaky@yamaworld.fr>
2016-10-30 23:05:43 -0400

    Add read example

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ BOARD_TAG := uno
 ARDMK_VENDOR := archlinux-arduino
 ARDUINO_LIBS := SPI CAN_BUS
 USER_LIB_PATH := $(realpath libraries)
+MONITOR_CMD := picocom
 
 CXXFLAGS += -Wall -Wextra
 
--- main.ino
+++ main.ino
@@ -1,10 +1,53 @@
-void setup() {
-    Serial.begin(9600);
-    Serial.print("Press any key: ");
+#include <SPI.h>
+#include "mcp_can.h"
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
 }
 
-void loop() {
-    if (Serial.available()) {
-        Serial.println(Serial.read());
+void printHex(INT8U item) {
+    Serial.print("0x");
+    Serial.print(item, HEX);
+}
+
+void loop()
+{
+    INT8U len = 0;
+    INT8U buf[8];
+
+    if (CAN_MSGAVAIL == CAN.checkReceive()) {
+        CAN.readMsgBuf(&len, buf);
+
+        Serial.print("{\"id\": 0x");
+        printHex(CAN.getCanId());
+        Serial.print(", \"isExtended\": ");
+        Serial.print(CAN.isExtendedFrame());
+        Serial.print(", \"isRemote\": ");
+        Serial.print(CAN.isRemoteRequest());
+
+        Serial.print(", \"data\": [");
+        if len > 0 {
+            printHex(buf[0])
+            for(size_t i = 1; i < len; i++) {
+                Serial.print(", ");
+                printHex(buf[i]);
+            }
+        }
+        Serial.println("]}");
     }
 }
